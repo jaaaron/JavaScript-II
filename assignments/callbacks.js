2dde1e4d4d525bdc35b812dd23a09b5ced658191
james <xjamesaaron@yahoo.com>
2019-08-21 09:09:15 -0700

    0.2

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,21 +1,17 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
-
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 console.log(items[3]);
-
 
   // GIVEN THIS PROBLEM:
 
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
   // SOLUTION:
 
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
-
   // NOTES ON THE SOLUTION:
 
   // firstItem is a higher order function.
@@ -42,23 +38,39 @@ console.log(items[3]);
 function getLength(arr, cb) {
   return cb(arr.length);// getLength passes the length of the array into the callback.
 }
+getLength(items, function(length) {
+  console.log(length);
+})
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);// last passes the last item of the array into the callback.
 }
+last(items, function(last) {
+  console.log(last);
+})
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return (x + y);// sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+console.log(sumNums(5,5, sumNums));
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return (x * y);// multiplyNums multiplies two numbers and passes the result to the callback.
 }
+console.log(multiplyNums(5,5,multiplyNums));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item) === true) {
+    return true
+  } else {
+    return false
+  }
 }
+console.log(contains("Pencil", items));
+console.log(contains("Hamburger", items));
+
 
 /* STRETCH PROBLEM */
 
